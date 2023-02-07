5853a7cc4120792d001eaaa0e1b9dae4e748a875
Scott Milner <scottdmilner@gmail.com>
2023-02-07 09:39:38 -0700

    initial config

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+module.exports = function override(config, env) {
+    const newRule = {
+      test: /\.txt$/i,
+      loader: 'raw-loader',
+      options: {
+        esModule: false,
+      },
+    }
+    config.module.rules.find((r) => r.oneOf).oneOf.unshift(newRule)
+    return config
+  }
