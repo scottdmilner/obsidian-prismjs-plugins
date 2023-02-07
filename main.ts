4c5fd3413682e611733e48a75fb179045d709e9f
Scott Milner <scottdmilner@gmail.com>
2023-02-07 09:39:19 -0700

    initial commit

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownPostProcessorContext, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+// @ts-ignore
+import lineNumbers from './line-numbers.js.txt'
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -10,72 +11,55 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
+
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
-
+	
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		const waitForPrism = (resolve: (val: typeof global.Prism) => void) => {
+			if (global?.Prism) 
+				resolve(global.Prism);
+			else 
+				setTimeout(waitForPrism.bind(waitForPrism, resolve), 100);
+		}
+		
+		const waitForLineNumbers = (resolve: (val: typeof global.Prism.plugins.lineNumbers) => void) => {
+			if (global.Prism?.plugins?.lineNumbers) 
+				resolve(global.Prism.plugins.lineNumbers);
+			else 
+				setTimeout(waitForLineNumbers.bind(waitForLineNumbers, resolve), 30);
+		}
+
+		this.registerMarkdownPostProcessor((element: HTMLElement, context: MarkdownPostProcessorContext) => {
+			const codeblocks = element.querySelectorAll("pre");
+			codeblocks.forEach(pre => {
+				if (pre.children[0].tagName == 'CODE'
+					&& !pre.classList.contains('line-numbers')) {
+						pre.classList.add('line-numbers');
+				}
+			});
+
+			if (codeblocks.length) {
+				new Promise(waitForPrism).then((Prism: typeof global.Prism) => Prism.highlightAll());
+			}
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
+		new Promise(waitForPrism).then((Prism: typeof global.Prism) => {
+			const script = document.createElement("script");
+			script.textContent = lineNumbers;
+			script.id = 'better-prism';
+			document.body.appendChild(script);
+
+			new Promise(waitForLineNumbers).then((lineNumbers) => {
+				lineNumbers.assumeViewportIndependence = false;
+				Prism.highlightAll();
+			});
+		});
 	}
 
 	onunload() {
@@ -91,21 +75,16 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+// 	onOpen() {
+// 		const {contentEl} = this;
+// 		contentEl.setText('Woah!');
+// 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+// 	onClose() {
+// 		const {contentEl} = this;
+// 		contentEl.empty();
+// 	}
+// }
 
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
